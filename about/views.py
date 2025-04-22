7c63f5b25121db6780234c7f4e8a477ec29950f2
Mehtap Hisarciklilar <68424810+hisarciklilar@users.noreply.github.com>
2025-04-22 22:06:37 +0100

    Add request method to views

--- about/views.py
+++ about/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def about_me(request):
-    return HttpResponse("This would be the about page")
-
+    if request.method == 'POST':
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
+    
