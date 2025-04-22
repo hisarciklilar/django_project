9fbc5587b0e4bc246a0138cc9c88e4f39fe3c343
Mehtap Hisarciklilar <68424810+hisarciklilar@users.noreply.github.com>
2025-04-22 22:08:11 +0100

    Correct changes in views.py

--- about/views.py
+++ about/views.py
@@ -3,8 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def about_me(request):
-    if request.method == 'POST':
-        return HttpResponse("You must have POSTed something")
-    else:
-        return HttpResponse(request.method)
-    
+    return HttpResponse("This would be the about page")
+
--- hello_world/views.py
+++ hello_world/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the hello_world index.")
-
-
+    if request.method == 'POST':
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
